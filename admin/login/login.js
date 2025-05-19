5dea64c8ac66b63b337e33a738a5a858728f15dc
hellounmesh <joker.mask.com@gmail.com>
2025-05-19 10:55:31 +0530

    Update login.js

--- admin/login/login.js
+++ admin/login/login.js
@@ -1,38 +1,38 @@
 const baseURL = "https://lynqdxb.onrender.com"
-const firebaseConfig = {
-    apiKey: "AIzaSyBHdUEAia7NTucKGIrqPBMSqXV9q0s6mkk",
-    authDomain: "notify-4c612.firebaseapp.com",
-    projectId: "notify-4c612",
-    storageBucket: "notify-4c612.firebasestorage.app",
-    messagingSenderId: "1098548785389",
-    appId: "1:1098548785389:web:5e7ac1b6c981b116b31ce4",
-    measurementId: "G-T355E98TWG"
-  };
-  const app = firebase.initializeApp(firebaseConfig);
-  const messaging = firebase.messaging();
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBHdUEAia7NTucKGIrqPBMSqXV9q0s6mkk",
+//     authDomain: "notify-4c612.firebaseapp.com",
+//     projectId: "notify-4c612",
+//     storageBucket: "notify-4c612.firebasestorage.app",
+//     messagingSenderId: "1098548785389",
+//     appId: "1:1098548785389:web:5e7ac1b6c981b116b31ce4",
+//     measurementId: "G-T355E98TWG"
+//   };
+//   const app = firebase.initializeApp(firebaseConfig);
+//   const messaging = firebase.messaging();
 
 
 const loginBtn = document.getElementById("loginBtn")
 const handleLogin = async() => {
 
     try {
-        await Notification.requestPermission();
-        const token = await messaging.getToken({
-            vapidKey: 'BNiclUBKzK7-Then1Sa32MeqIECyMRdXo_SZl6koWcgrVVMhMdJpJwaVsj2I2NQbxIQiL48UTbwOpdOkD2AtIPQ',
-            serviceWorkerRegistration: await navigator.serviceWorker.register('../firebase-messaging-sw.js')
-          });
+        // await Notification.requestPermission();
+        // const token = await messaging.getToken({
+        //     vapidKey: 'BNiclUBKzK7-Then1Sa32MeqIECyMRdXo_SZl6koWcgrVVMhMdJpJwaVsj2I2NQbxIQiL48UTbwOpdOkD2AtIPQ',
+        //     serviceWorkerRegistration: await navigator.serviceWorker.register('../firebase-messaging-sw.js')
+        //   });
 
-          if (!token) {
-            alert("Unable to get device token. Notifications may be blocked.");
-            return;
-        }
+        //   if (!token) {
+        //     alert("Unable to get device token. Notifications may be blocked.");
+        //     return;
+        // }
         const email = document.getElementById("Email").value.trim();
         const password = document.getElementById("Password").value.trim();
 
         const payload = {
             email,
             password,
-            deviceToken:token,
+          
             platform:"web",
             deviceId:generateDeviceId(),
 
